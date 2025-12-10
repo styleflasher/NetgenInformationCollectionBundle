7e848fe581a44e59d9519b8b3c828904d673a63e
Edi Modrić <edi.modric@gmail.com>
2025-12-10 11:20:52 +0100

    Use netgen/coding-standard

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,45 +1,11 @@
 <?php
 
-return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
-    ->setRules([
-        '@PhpCsFixer' => true,
-        '@PhpCsFixer:risky' => true,
+// To support running PHP CS Fixer via PHAR file (e.g. in GitHub Actions)
+require_once __DIR__ . '/vendor/netgen/coding-standard/lib/PhpCsFixer/Config.php';
 
-        // Overrides for rules included in PhpCsFixer rule sets
-        'concat_space' => ['spacing' => 'one'],
-        'method_chaining_indentation' => false,
-        'multiline_whitespace_before_semicolons' => false,
-        'native_function_invocation' => ['include' => ['@all']],
-        'no_superfluous_phpdoc_tags' => false,
-        'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
-        'php_unit_internal_class' => false,
-        'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
-        'php_unit_test_class_requires_covers' => false,
-        'phpdoc_align' => false,
-        'phpdoc_types_order' => ['null_adjustment' => 'always_last', 'sort_algorithm' => 'none'],
-        'single_line_comment_style' => false,
-        'visibility_required' => ['elements' => ['property', 'method', 'const']],
-        'yoda_style' => false,
-
-        // Additional rules
-        'date_time_immutable' => true,
-        'declare_strict_types' => true,
-        'global_namespace_import' => [
-            'import_classes' => null,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
-        'list_syntax' => ['syntax' => 'short'],
-        'mb_str_functions' => true,
-        'native_constant_invocation' => true,
-        'nullable_type_declaration_for_default_null_value' => true,
-        'static_lambda' => true,
-        'ternary_to_null_coalescing' => true,
-    ])
+return (new Netgen\CodingStandard\PhpCsFixer\Config())
     ->setFinder(
         PhpCsFixer\Finder::create()
-            ->exclude(['vendor', 'node_modules', 'doc', 'bundle'])
-            ->in(__DIR__)
+            ->in(['bundle', 'lib'])
     )
 ;
